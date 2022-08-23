fddc37fc4f6838543d416edbbe4d339be1e5d87d
Roy Schrauwen <roy@royschrauwen.nl>
2022-08-23 16:12:21 +0200

    ID is folename

--- inspection.php
+++ inspection.php
@@ -90,7 +90,7 @@ $inspection = new Inspection(
 
 
 $mpdf = new \Mpdf\Mpdf(['setAutoTopMargin' => 'stretch']);
-$mpdf->SetTitle($inspection->get('registratienummer'));
+$mpdf->SetTitle($inspection->get('id'));
 
 $stylesheet = file_get_contents('style.css');
 $mpdf->WriteHTML($stylesheet,\Mpdf\HTMLParserMode::HEADER_CSS);
@@ -239,98 +239,4 @@ if(count($finding->get('followUpActions')) > 0) {
 }
 
 
-
-// // Afbeeldingen, max 2 per rij
-// if(count($inspection->get('afbeeldingen')) > 0) {
-//     $mpdf->WriteHTML('<table class="rapport-afbeeldingen rapport-section">');
-//     foreach (array_chunk($inspection->get('afbeeldingen'), 2) as $row) {
-//         $mpdf->WriteHTML('<tr>');
-//         foreach ($row as $value) { 
-//             $mpdf->WriteHTML('
-//             <td><center>
-//                 <img class="rapport-afbeelding" src="' . $value . '" alt="">
-//             </center></td>
-//             ');
-//         } 
-//         $mpdf->WriteHTML('</tr>');
-//     }
-//     $mpdf->WriteHTML('</table>');
-// }
-
-
-// $mpdf->WriteHTML('
-//     <table class="rapport-section">
-//         <tr>
-//             <td><b>Omschrijving directe Aanleiding</b><br>
-//             ' . nl2br($inspection->get('omschrijvingDirecteAanleiding')) . '</td>
-//         </tr>
-//         <tr>
-//             <td><b>Oorzaakanalyse</b><br>
-//             ' . nl2br($inspection->get('oorzaakanalyse')) . '</td>
-//         </tr>
-//         <tr>
-//             <td><b>Omvanganalyse van de constatering</b><br>
-//             ' . nl2br($inspection->get('omvanganalyse')) . '</td>
-//         </tr>
-//         <tr>
-//             <td><b>Verbetervoorstel</b><br>
-//             ' . nl2br($inspection->get('verbetervoorstel')) . '</td>
-//         </tr>
-//         <tr>
-//             <td><b>Reeds genomen acties</b><br>
-//             ' . nl2br($inspection->get('reedsGenomenActies')) . '</td>
-//         </tr>
-//         <tr>
-//             <td><b>Plandatum</b><br>
-//             ' . $inspection->get('plandatum') . '</td>
-//         </tr>
-//     </table>
-// ');
-
-
-// // Vervolgacties indien aanwezig
-// if(count($inspection->get('vervolgacties')) > 0) {
-
-//     $mpdf->WriteHTML('
-//     <p style="margin-left: 0.25rem"><b>Vervolgacties</b></p>
-//     ');
-
-//     for ($i=0; $i < count($inspection->get('vervolgacties')) ; $i++) { 
-//         $mpdf->WriteHTML('
-//         <table class="vervolgacties">
-//             <tr>
-//                 <td colspan="5"><b>Vervolgactie ' . $i+1 . '</b></td>
-//             </tr>
-//             <tr class="vervolgactie-omschrijving">
-//                 <td colspan="5">' . $inspection->get('vervolgacties')[$i]['actie'] . '</td>
-//             </tr>
-//             <tr>
-//                 <td><b>Intern/extern</b></td>
-//                 <td><b>Voorgestelde actiehouder</b></td>
-//                 <td><b>Daadwerkelijke actiehouder</b></td>
-//                 <td><b>Plandatum</b></td>
-//             </tr>
-//             <tr>
-//                 <td>' . $inspection->get('vervolgacties')[$i]['internExtern'] . '</td>
-//                 <td>' . $inspection->get('vervolgacties')[$i]['voorgesteldeActiehouder'] . '</td>
-//                 <td>' . $inspection->get('vervolgacties')[$i]['daadwerkelijkeActiehouder'] . '</td>
-//                 <td>' . $inspection->get('vervolgacties')[$i]['plandatum'] . '</td>
-//             </tr>
-//         </table>
-//         ');
-//     }
-// }
-
-// $mpdf->WriteHTML('
-//     <table class="rapport-section">
-//         <tr>
-//             <td><b>Doeltreffendheid van de actie</b></td>
-//         </tr>
-//         <tr>
-//             <td>' . $inspection->get('doeltreffendheid') . '</td>
-//         </tr>
-//     </table>
-// </div>
-// ');
-
-$mpdf->Output($inspection->get('registratienummer').'.pdf', 'I');
+$mpdf->Output($inspection->get('id').'.pdf', 'I');
